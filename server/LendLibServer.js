72a2e448d1230ec16aaa275a6b62457f715d3b06
Isaac Strack <strack@gmail.com>
2015-07-03 00:13:52 -0600

    2nd Edition

--- server/LendLibServer.js
+++
@@ -1,8 +0,0 @@
-  Meteor.startup(function () {
-    Meteor.publish("Categories", function() {
-      return lists.find({owner:this.userId},{fields:{Category:1}});
-    });
-    Meteor.publish("listdetails", function(category_id){
-      return lists.find({_id:category_id});
-    });
-  });
